93a7dbb329c25ec3e057c019c90d546fce2618d2
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-09-09 23:46:42 -0500

    backend setup

---
+++ backend/src/main.ts
@@ -0,0 +1,15 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+
+async function bootstrap() {
+  dotenv.config();
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    methods: 'GET, POST, PUT, DELETE, OPTIONS, PATCH',
+  });
+
+  await app.listen(process.env.PORT || 5000);
+}
+bootstrap();
