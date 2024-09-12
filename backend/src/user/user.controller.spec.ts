6cd71a771c3692433cb803fe85219582c78e5fa1
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-09-12 17:38:54 -0500

    fixed issues

--- backend/src/user/user.controller.spec.ts
+++ backend/src/user/user.controller.spec.ts
@@ -1,17 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { InsuranceController } from './user.controller';
-import { InsuranceService } from './user.service';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
 
-describe('InsuranceController', () => {
-  let controller: InsuranceController;
+describe('UserController', () => {
+  let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [InsuranceController],
-      providers: [InsuranceService],
+      controllers: [UserController],
+      providers: [UserService],
     }).compile();
 
-    controller = module.get<InsuranceController>(InsuranceController);
+    controller = module.get<UserController>(UserController);
   });
 
   it('should be defined', () => {
