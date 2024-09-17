e95204b5a4e720fb3fbd78611d80289b4eb65892
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-09-17 11:57:53 -0500

    home page mvp design

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,34 @@
 'use client'
 import React from 'react';
+import Link from "next/link";
 
 const Home: React.FC = () => {
 
     return (
         <>
-
+            <h1 className="text-2xl font-semibold text-center">Welcome to the Home Page</h1>
+            <div className="flex flex-row items-center mt-4 justify-center">
+              <Link href="/signin">
+                  <div className="flex justify-center">
+                      <button className="bg-blue-500 text-white p-2 rounded m-4">Sign In</button>
+                  </div>
+              </Link>
+              <Link href="/signup">
+                  <div className="flex justify-center">
+                      <button className="bg-blue-500 text-white p-2 rounded m-4">Sign Up</button>
+                  </div>
+              </Link>
+              <Link href="/reset-password">
+                  <div className="flex justify-center">
+                      <button className="bg-blue-500 text-white p-2 rounded m-4">Reset Password</button>
+                  </div>
+              </Link>
+              <Link href="/forgot-password">
+                  <div className="flex justify-center">
+                      <button className="bg-blue-500 text-white p-2 rounded m-4">Forgot Password</button>
+                  </div>
+              </Link>
+            </div>
         </>
     );
 }
