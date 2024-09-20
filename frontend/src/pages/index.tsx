d23894741f93d0b4d57c7a8f6a68452213486eb6
jlee927 <jlee927@uic.edu>
2024-09-20 12:42:07 -0500

    added parts of landing page

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,14 +3,27 @@ import Link from "next/link";
 import React from "react";
 
 const Home: React.FC = () => {
-
     return (
-            <div>
-                <h1>Find the most affordable medical services</h1>
-                <h2>Healthcare Connect provides progressive, and affordable healthcare, accessible on mobile and online for everyone</h2>
-                <button>Get Started</button>
+        <>
+            <div className="flex flex-col border border-solid p-16 mt-32">
+                <h1 className="text-6xl font-bold">
+                    Find the most affordable
+                    <br />
+                    medical services
+                </h1>
+                <h2 className="text-lg mt-4 text-gray-400">
+                    Healthcare Connect provides progressive, and affordable
+                    <br />
+                    healthcare, accessible on mobile and online
+                    <br />
+                    for everyone
+                </h2>
+                <button className="w-44 h-12 mt-4 bg-blue-500 text-white px-4 py-2 rounded-3xl">
+                    Get Started
+                </button>
             </div>
-    );
+        </>
+    )
 }
 
-export default Home;
+export default Home
