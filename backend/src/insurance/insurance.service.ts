a85023c4e2a1cd4a039b155c62a5fe06ba79a0b9
Disguied-Coffee <danielidanque@gmail.com>
2024-09-20 09:05:57 -0500

    Cleaned up code for future reference. Thanks Arslan!

--- backend/src/insurance/insurance.service.ts
+++ backend/src/insurance/insurance.service.ts
@@ -5,16 +5,17 @@ import { IInsurancePlan, InsurancePlan } from './insurance.schema';
 export class InsuranceService {
 
     /**
-     * get all the insurance plans
+     * Gets all the insurance plans from DB
+     * 
      * @returns InsurancePlan array
      */
     async getAll() {
         return InsurancePlan.find();
     }
 
-    // get an insurance plan by ID
-
     /**
+     * 
+     * Retrieves an InsurancePlan schema by ID.
      * 
      * @param id string id of the plan
      * @returns InsurancePlan schema in question
@@ -23,16 +24,12 @@ export class InsuranceService {
         return InsurancePlan.findById(id);
     }
 
-    // delete an insurance plan by ID
-
-    // 
     /**
-     *  ***Needs testing []***
      * 
-     *  "I tried deleting, but it never worked... Maybe a priviledge issue?"
+     * Deletes an insurance plan schema from DB
      * 
      * @param id sting id of schema to delete
-     * @returns ([] Should there be certain codes?)
+     * @returns nil []
      */
     async delete(id: string) {
         await InsurancePlan.findByIdAndDelete(id);
@@ -42,34 +39,25 @@ export class InsuranceService {
      * Creates insurance plan schema in DB
      * 
      * @param data IInsurancePlan schema
-     * @returns 1 ([] Needs a try-except maybe?)
+     * @returns a copy of the schema with datestamps and ID
      */
     async create(data: IInsurancePlan){
+        //Create a new insurance plan with the schema of data we got.
         const newInsurancePlan = new InsurancePlan(data);
+        // and send a copy of the data from the DB,
+        // after saving it into the DB of course. 
         return newInsurancePlan.save();
     }
     
     /**
      * 
-     * ***Needs testing []***
-     * 
      * Updates an insurance plan via string id
      * 
      * @param id string id of insurance plan to update
-     * @param data data to update the plan with
-     * @returns 
+     * @param data IInsurancePlan data schema to update the plan with
+     * @returns nil []
      */
     async update(id: string, data: IInsurancePlan){
         await InsurancePlan.findByIdAndUpdate(id, data);
     }
 }   
-    // async create(insurance: any) {
-    //     try {
-    //         const newInsurance = new InsurancePlan(insurance);
-    //         return await newInsurance.save();
-    //     } catch (error: any) {
-    //         return error;
-    //     }
-    // }
-
-}
