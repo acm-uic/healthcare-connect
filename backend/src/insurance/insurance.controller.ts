cc35f79c425f58f39a5ab1dfbd30b66434ad31e0
Disguied-Coffee <danielidanque@gmail.com>
2024-09-19 20:16:41 -0500

    Finished task, waiting for approval

--- backend/src/insurance/insurance.controller.ts
+++ backend/src/insurance/insurance.controller.ts
@@ -1,8 +1,36 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Delete, Post, Put, Param, Body } from '@nestjs/common';
 import { InsuranceService } from './insurance.service';
+import { IInsurancePlan } from './insurance.schema';
 
 @Controller('insurance')
 export class InsuranceController {
   constructor(private readonly insuranceService: InsuranceService) {}
 
+  @Get()
+  async getAllInsurancePlans(){
+    return this.insuranceService.getAll();
+  }
+
+  @Get('/:id')
+  async getInsurancePlanByID(@Param('id') id:string){
+    return this.insuranceService.get(id);
+  }
+
+
+  @Delete('/:id') //delete
+  async deleteSpecificInsurancePlan(@Param('id') id:string){
+    return this.insuranceService.delete(id)
+  }
+
+  
+  @Post()
+  async createInsurancePlan(@Body() data: IInsurancePlan){
+    return this.insuranceService.create(data)
+  }
+
+  @Put('/:id')
+  async updateInsurancePlan(@Param('id') id:string, @Body() data: IInsurancePlan){
+    return this.insuranceService.update(id,data)
+
+  }
 }
--- backend/src/insurance/insurance.service.ts
+++ backend/src/insurance/insurance.service.ts
@@ -1,6 +1,70 @@
 import { Injectable } from '@nestjs/common';
+import { IInsurancePlan, InsurancePlan } from './insurance.schema';
+
 
 @Injectable()
 export class InsuranceService {
 
-}
+    /**
+     * get all the insurance plans
+     * @returns InsurancePlan array
+     */
+    async getAll() {
+        return InsurancePlan.find();
+    }
+
+    // get an insurance plan by ID
+
+    /**
+     * 
+     * @param id string id of the plan
+     * @returns InsurancePlan schema in question
+     */
+    async get(id: string) {
+        return InsurancePlan.findById(id);
+    }
+
+    // delete an insurance plan by ID
+
+    // 
+    /**
+     *  ***Needs testing []***
+     * 
+     *  "I tried deleting, but it never worked... Maybe a priviledge issue?"
+     * 
+     * @param id sting id of schema to delete
+     * @returns ([] Should there be certain codes?)
+     */
+    async delete(id: string) {
+        InsurancePlan.findByIdAndDelete(id);
+        console.log(`Deleted insurance plan id ${id}...`)
+        return "BANNED!"
+    }
+
+    /**
+     * Creates insurance plan schema in DB
+     * 
+     * @param data IInsurancePlan schema
+     * @returns 1 ([] Needs a try-except maybe?)
+     */
+    async create(data: IInsurancePlan){
+        InsurancePlan.create(data);
+        return 1
+    }
+
+    
+    /**
+     * 
+     * ***Needs testing []***
+     * 
+     * Updates an insurance plan via string id
+     * 
+     * @param id string id of insurance plan to update
+     * @param data data to update the plan with
+     * @returns 
+     */
+    async update(id:string, data: IInsurancePlan){
+        InsurancePlan.findByIdAndUpdate(id, data);
+        return 1
+    }
+}   
