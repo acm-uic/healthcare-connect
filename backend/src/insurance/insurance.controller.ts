7a1351fe0bfd01c6c45de027cd25d048c769194d
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-09-19 19:20:55 -0500

    task setup

--- backend/src/insurance/insurance.controller.ts
+++ backend/src/insurance/insurance.controller.ts
@@ -1,8 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { InsuranceService } from './insurance.service';
+import { IInsurancePlan } from './insurance.schema';
 
 @Controller('insurance')
 export class InsuranceController {
   constructor(private readonly insuranceService: InsuranceService) {}
 
+  // @Post()
+  // async createInsurance(@Body() insurance: IInsurancePlan) {
+  //   return this.insuranceService.create(insurance);
+  // }
 }
--- backend/src/insurance/insurance.service.ts
+++ backend/src/insurance/insurance.service.ts
@@ -4,4 +4,13 @@ import { InsurancePlan } from './insurance.schema';
 @Injectable()
 export class InsuranceService {
 
+    // async create(insurance: any) {
+    //     try {
+    //         const newInsurance = new InsurancePlan(insurance);
+    //         return await newInsurance.save();
+    //     } catch (error: any) {
+    //         return error;
+    //     }
+    // }
+
 }
