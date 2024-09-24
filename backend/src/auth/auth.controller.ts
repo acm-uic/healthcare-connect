e7b35e924032aed5e43e07693f86b0572701bfd4
Disguied-Coffee <danielidanque@gmail.com>
2024-09-24 09:33:31 -0500

    attempt 1, needs testing

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,21 +1,24 @@
-import { Controller, Post, Req, Res, Next } from '@nestjs/common';
+import { Controller, Post, Req, Res, Next, Body } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 import { AuthService } from './auth.service';
 import { User } from '../user/user.schema'
 import * as jwt from 'jsonwebtoken';
 import sendEmail from '../utils/sendMail';
 
+import bcrypt from "bcrypt";
+
+
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) { }
 
   @Post('signup')
-  async signup(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction){
+  async signup(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction) {
     const { email, password } = req.body;
 
     const user = await User.findOne({ email });
-    if(user) return res.status(400).json({ message: 'User already exists' });
-    
+    if (user) return res.status(400).json({ message: 'User already exists' });
+
     const newUser = { email, password };
 
     const activationToken = await this.generateActivationToken(newUser);
@@ -40,5 +43,49 @@ export class AuthController {
     const code = Math.floor(1000 + Math.random() * 9000).toString();
     const token = jwt.sign({ email: user.email, code }, process.env.JWT_SECRET as string, { expiresIn: '10m' });
     return token;
+  }
+
+
+  /**
+   * 
+   * NEEDS TESTING []
+   * 
+   * @param req 
+   * @param res 
+   * @param next
+   * @returns 
+   */
+  @Post('signin')
+  async signin(@Req() req: Request, @Res() res: Response, @Next() next: NextFunction) {
+    // If there is an error that occurs in any of this, send an internal server error.
+    try {
+      // Hmm
+      const { email, password } = req.body;
+      // find the user
+      const user = await User.findOne({ email });
+
+      // Check: email not found error:
+      if (!user) {
+        return res.status(400).json({ message: 'User email not found' });
+      }
+
+      // user.password is a hash...
+      // call back can be added here []
+      if(bcrypt.compare(password, user.password)){
+        console.log("sucessful login of " + email);
+
+        //if successful return user id and role.
+        return {
+          "User role": user.role,
+          "User ID": user.id
+        }
+      }
+      // if not, just 
+      return res.status(401).json({message: 'Password Incorrect'});
+    }
+    catch (error) {
+      console.log("Error has occured in auth.controller: " + error)
+      return res.status(500).json({ message: 'An internal server error has occured, please notify Dev Team.' });
+    }
   }
 }
