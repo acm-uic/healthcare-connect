4a0a37d91fca6a46d131fd4273b6c3d3a2d6e209
eascen4 <eascen4official@gmail.com>
2024-09-24 16:31:58 -0500

    Added: - JWT Strategy - JWT Payload class - validateByUserId in AuthService - installed passport-jwt types

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,10 +1,17 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
-import { AuthService } from './auth.service';
+import { Module } from "@nestjs/common";
+import { AuthController } from "./auth.controller";
+import { AuthService } from "./auth.service";
+import { JwtModule } from "@nestjs/jwt";
+import { JwtStrategy } from "./strategies/jwt.strategy";
 
 @Module({
-  imports: [],
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: Number(process.env.JWT_EXPIRE) },
+    }),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,6 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { JwtPayload } from "./interfaces/jwt.payload";
+
+//Temporary until UserService is implemented
+import { User } from "src/user/user.schema";
+import { isValidObjectId } from "mongoose";
 
 @Injectable()
 export class AuthService {
-  
+  constructor(private jwtService: JwtService) {}
+
+  async validateUserById(userId: string): Promise<boolean> {
+
+    if (!isValidObjectId(userId)) return false;
+
+    const user = await User.findById(userId);
+
+    return !!user;
+  }
 }
