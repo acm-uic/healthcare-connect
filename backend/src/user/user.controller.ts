91403ada5a6181f1fba38f262e2f1becb8ff8292
Dhruv Patel <dhruvthakorpatel05@gmail.com>
2024-09-24 21:36:40 -0500

    Save Service Route

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,28 @@
-import { Controller, Req, Get, Post, Put, Delete, Param, Body, UseGuards } from '@nestjs/common';
+import { Controller, Req, Get, Post, Put, Delete, Param, Body, UseGuards, Res } from '@nestjs/common';
 import { UserService } from './user.service';
+import { response } from 'express';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  
+  @Post('/:serviceId')  
+  async saveService(
+  @Res() response,
+   @Param('userId') userId: string,
+   @Param('serviceId') serviceId: string,
+  )
+  {
+    const savedService = this.userService.save(userId, serviceId);
+    return response.status(201).json({
+      message: 'Service successfully saved', savedService
+    })
+  } catch (err)
+  {
+    return response.status(400).json(
+      {
+        message: 'Error: Service not saved'
+      }
+    )
+  }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,9 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { User } from './user.schema';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
+import { IUser, User } from './user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class UserService {
+    constructor(@InjectModel('User') private userModel: Model<IUser>) {}
+    async save(userId: string, serviceId: string): Promise<IUser>
 
-    
-   
+    {
+        // Checks if the user exists by ID
+        const user = await this.userModel.findById(userId);
+        if (!user) 
+            {
+                throw new NotFoundException('User not found');
+            }
+
+        //Checks if the service already exists by ID
+        if (user.savedServices.includes(serviceId))
+            {
+                throw new BadRequestException('Service already exists');
+            }
+
+        //Adds the service ID to user profile
+        user.savedServices.push(serviceId);
+        await user.save();
+        return user;
+    } 
 }
