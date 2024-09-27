0dbd94a194254cfebd458d8d72cca743e53736f2
jlee927 <jlee927@uic.edu>
2024-09-27 12:02:49 -0500

    Added displayServices component to show all of the services that were fetched from services backend. The buttons send users to home for now but will show more details in the future

--- frontend/src/pages/services/index.tsx
+++ frontend/src/pages/services/index.tsx
@@ -1,9 +1,9 @@
-
+import DisplayServices from "@/components/DisplayServices";
 
 const Services = () => {
     return (
         <div>
-            <h1>Services</h1>
+            <DisplayServices/>
         </div>
     );
 }
