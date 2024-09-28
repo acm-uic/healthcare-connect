6f3bf2c0cd775d492ba79b18850a3f005f1d4962
Arslan Kamchybekov <arslankamcybekov7@gmail.com>
2024-09-28 14:51:14 -0500

    insurance: filtering by monthlyPremium

--- backend/src/insurance/insurance.controller.ts
+++ backend/src/insurance/insurance.controller.ts
@@ -1,10 +1,30 @@
-import { Controller, Get, Delete, Post, Put, Param, Body } from '@nestjs/common';
+import { Controller, Get, Delete, Post, Put, Param, Body, Query, Req, Res } from '@nestjs/common';
 import { InsuranceService } from './insurance.service';
 import { IInsurancePlan } from './insurance.schema';
 
-@Controller('insurance')
+@Controller('insurance-plan')
 export class InsuranceController {
   constructor(private readonly insuranceService: InsuranceService) {}
+
+  @Get('filter')
+  async filterInsurancePlans(
+    @Req() req,
+    @Res() res,
+    @Query() query: { minPrice: number; maxPrice: number },
+  ) {
+    try {
+      const insurancePlans = await this.insuranceService.getAll();
+      const filteredPlans = insurancePlans.filter((plan) => {
+        return (
+          plan.monthlyPremium <= query.maxPrice &&
+          plan.monthlyPremium >= query.minPrice
+        );
+      });
+      return res.status(200).json(filteredPlans);
+    } catch (error) {
+      return res.status(400).json({ message: 'Error: Insurance plans not found' });
+    }
+  }
 
   @Get()
   async getAllInsurancePlans(){
@@ -30,6 +50,5 @@ export class InsuranceController {
   @Put(':id')
   async updateInsurancePlan(@Param('id') id: string, @Body() data: IInsurancePlan){
     return this.insuranceService.update(id,data)
-
   }
 }
