679bde4f04028fa141fba96dbebc469fa092c24a
Disguied-Coffee <danielidanque@gmail.com>
2024-09-29 10:15:05 -0500

    tiny fix

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -33,16 +33,3 @@ export class AuthService {
     return token;
   }
 }
-
-/**
- * _***Sign In error messages in one spot!!***_
- */
-export const AuthClientSideErrorMessages = {
-    internalError:"An internal server error has occured, please notify Dev Team.",
-    incorrectPwd: "Password Incorrect",
-    emailNotFound: "User email not found",
-    noPwd:"Missing Password!",
-    noEmail:"Missing Email!"
-}
-
-
