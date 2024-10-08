5955cdb048d0fda9ecfb43520d1761a3842d8f13
schenna217 <90796839+schenna217@users.noreply.github.com>
2024-10-08 00:55:33 -0500

    Initial Setup of Insurance Page

---
+++ frontend/src/pages/insurance.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Insurance: React.FC = () => {
+
+    return(
+        <div>
+            <h1>This is the initial setup for the insurance page.</h1>
+        </div>
+    );
+
+}
+
+export default Insurance;
