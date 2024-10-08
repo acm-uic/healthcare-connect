16a64a5d74085f5a4cb6529c0c1902a49e724367
Shaheer Rasheed <shaheer.football.5@gmail.com>
2024-10-07 21:43:00 -0500

    Add index.tsx for insurances page

---
+++ frontend/src/pages/insurances/index.tsx
@@ -0,0 +1,12 @@
+import DisplayInsaurances from "@/components/DisplayInsaurances";
+import DisplayServices from "@/components/DisplayServices";
+
+const Services = () => {
+    return (
+        <div>
+            <DisplayInsaurances/>
+        </div>
+    );
+}
+
+export default Services;
